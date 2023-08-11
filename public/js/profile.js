18a98c6f23923e204d9f913e93bddeadbbb9f228
Merel Jacobs <merel.burleigh@gmail.com>
2023-08-11 08:04:39 -0700

    debug deleting comments from profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,6 @@
 
 const deleteBtns = document.querySelectorAll('.delete-btn');
+const deleteBtnComment = document.querySelectorAll('.delete-btn-comment')
 const title = document.querySelectorAll('.title')
 
 // delete a post successful
@@ -24,6 +25,27 @@ deleteBtns.forEach((deleteBtn) => {
     })
 })
 
+
+deleteBtnComment.forEach((deleteBtnComment) => {
+    deleteBtnComment.addEventListener('click', (event) => {
+
+        let commentId = event.currentTarget.parentElement.parentElement.id; 
+        console.log(commentId)
+        fetch(`/api/comment/${commentId}`, {
+            method: 'DELETE',
+            headers: {'Content-type': 'application/json'},
+        })
+        // send route again to render homepage
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+    
+        })
+        .catch ((err) => {
+            console.error(err)})
+    
+    })
+})
 
 title.forEach((title) => {
     title.addEventListener('click', (event) => {
