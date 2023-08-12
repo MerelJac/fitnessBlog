254f6aef624b9222dc325986be736a2be0ec8650
Merel Jacobs <merel.burleigh@gmail.com>
2023-08-12 16:03:25 -0700

    tailwindcss added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", 
+"./views/*.handlebars",
+"./views/layouts/*.handlebars"
+
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
